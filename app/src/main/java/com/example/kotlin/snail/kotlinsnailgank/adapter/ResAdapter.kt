e4bb9f7711fc7a507188288b7679b59223d65047
snail <m15142555427@163.com>
2017-07-31 16:58:01 +0800

    res

---
+++ app/src/main/java/com/example/kotlin/snail/kotlinsnailgank/adapter/ResAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.kotlin.snail.kotlinsnailgank.adapter
+
+import android.content.Context
+import android.util.SparseArray
+import android.view.View
+import android.widget.TextView
+import com.example.kotlin.snail.kotlinsnailgank.R
+import com.example.kotlin.snail.kotlinsnailgank.base.BaseAdapter
+import com.example.kotlin.snail.kotlinsnailgank.bean.ResBean
+
+/**
+ * Created by 张志强 on 2017/7/31.
+ */
+class ResAdapter<T>(context: Context) : BaseAdapter<T>(context) {
+    override fun addAllViewItems(viewMap: SparseArray<View>, view: View) {
+        viewMap.put(R.id.res_desc_tv, view.findViewById(R.id.res_desc_tv))
+        viewMap.put(R.id.res_auth_tv, view.findViewById(R.id.res_auth_tv))
+        viewMap.put(R.id.res_time_tv, view.findViewById(R.id.res_time_tv))
+    }
+
+    override fun getViewResource(viewType: Int): View {
+        return layoutInflater.inflate(R.layout.item_res_layout, null)
+    }
+
+    override fun bindData(viewMap: SparseArray<View>?, t: T?, position: Int) {
+        val resBean: ResBean = t as ResBean
+        (viewMap!![R.id.res_desc_tv] as TextView).text = resBean.desc
+        (viewMap!![R.id.res_auth_tv] as TextView).text = resBean.who
+        (viewMap!![R.id.res_time_tv] as TextView).text = resBean.createdAt
+    }
+}
--- app/src/main/java/com/example/kotlin/snail/kotlinsnailgank/fragment/ResFragment.kt
+++ app/src/main/java/com/example/kotlin/snail/kotlinsnailgank/fragment/ResFragment.kt
@@ -1,22 +1,41 @@
 package com.example.kotlin.snail.kotlinsnailgank.fragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.kotlin.snail.kotlinsnailgank.R
+import com.example.kotlin.snail.kotlinsnailgank.activity.WebViewActivity
+import com.example.kotlin.snail.kotlinsnailgank.adapter.ResAdapter
+import com.example.kotlin.snail.kotlinsnailgank.base.BaseAdapter
+import com.example.kotlin.snail.kotlinsnailgank.bean.IosBean
+import com.example.kotlin.snail.kotlinsnailgank.bean.ResBean
+import com.example.kotlin.snail.kotlinsnailgank.common.Constant
+import com.example.kotlin.snail.kotlinsnailgank.observable.ObservableHelper
+import com.example.kotlin.snail.kotlinsnailgank.view.SpaceItemDecoration
+import com.jcodecraeer.xrecyclerview.XRecyclerView
+import kotlinx.android.synthetic.main.fragment_ios.*
+import kotlinx.android.synthetic.main.fragment_res.*
+import rx.Observer
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class ResFragment : Fragment() {
+class ResFragment : Fragment(), XRecyclerView.LoadingListener, Observer<List<ResBean>> {
+
     private val TYPE = "拓展资源"
     private val COUNT = 10
     private var page = 0
+    private var adapter: ResAdapter<ResBean>? = null
+
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -24,4 +43,67 @@ class ResFragment : Fragment() {
         return inflater!!.inflate(R.layout.fragment_res, container, false)
     }
 
-}// Required empty public constructor
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initXRecyclerView()
+    }
+
+    private fun initXRecyclerView() {
+        var layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        res_xrv.layoutManager = layoutManager
+        //设置刷新风格
+        res_xrv.setRefreshProgressStyle(Constant.REFRESHSTYLE)
+        res_xrv.setLoadingMoreProgressStyle(Constant.LOADINGSTYLE)
+        res_xrv.addItemDecoration(SpaceItemDecoration(10))
+
+        adapter = ResAdapter<ResBean>(context).apply {
+            setRecyclerViewItemClickListener(object : BaseAdapter.RecyclerViewItemClickListener {
+                override fun onItemclick(view: View, position: Int) {
+                    val bean = adapter?.getItem(position) as ResBean
+                    startActivity(Intent(context, WebViewActivity::class.java).putExtra(Constant.OPENURL, bean.url))
+                }
+            })
+        }
+        res_xrv.adapter = adapter
+        res_xrv.setLoadingListener(this)
+        res_xrv.refresh()
+    }
+
+    override fun onLoadMore() {
+        loadData(++page)
+    }
+
+    override fun onRefresh() {
+        page = 1
+        loadData(page)
+    }
+
+    private fun loadData(page: Int) {
+        ObservableHelper.getResObservable(TYPE, COUNT, page)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this)
+    }
+
+    override fun onError(e: Throwable?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCompleted() {
+        if (page == 1) {
+            res_xrv.refreshComplete()
+        } else {
+            res_xrv.loadMoreComplete()
+        }
+    }
+
+    override fun onNext(t: List<ResBean>?) {
+        if (page == 1) {
+            adapter?.removewAll()
+            adapter?.setDataSet(t as List<ResBean>)
+        } else {
+            adapter?.addData(t as List<ResBean>)
+        }
+    }
+}
